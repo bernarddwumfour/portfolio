109fc40472edfc8cecf36b554c05eb5c8a57095b
bernarddwumfour25 <bernarddwumfour25@gmail.com>
2025-08-09 16:53:05 +0000

    fixing error 4

--- src/app/components/TypingAnimation.tsx
+++ src/app/components/TypingAnimation.tsx
@@ -10,7 +10,7 @@ interface TypingAnimationProps {
   deletionSpeed?: any;
   style?: React.CSSProperties;
   className?: string;
-  wrapper?: string;
+  wrapper?: any;
 }
 
 export default function TypingAnimation({
